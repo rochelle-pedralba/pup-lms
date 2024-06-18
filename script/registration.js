bd9354e0adbf6d694e95a76d2a4efd699346a94b
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-06-18 22:51:22 +0800

    Create registration.js

---
+++ script/registration.js
@@ -0,0 +1,19 @@
+function showForm2(id) {
+  document.getElementById("second_form").style.display = "flex";
+  document.getElementById(id).style.display = "none";
+}
+
+function showForm3(id) {
+  document.getElementById("third_form").style.display = "flex";
+  document.getElementById(id).style.display = "none";
+}
+
+function previousForm1(id) {
+  document.getElementById("first_form").style.display = "flex";
+  document.getElementById(id).style.display = "none";
+}
+
+function previousForm2(id) {
+    document.getElementById("second_form").style.display = "flex";
+    document.getElementById(id).style.display = "none";
+}
