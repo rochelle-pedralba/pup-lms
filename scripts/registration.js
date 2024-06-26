b3d0bd59c97e940835c27ffe6e1aa4beaacfaca8
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-06-26 15:52:21 +0800

    Update registration.js

--- scripts/registration.js
+++ scripts/registration.js
@@ -118,3 +118,25 @@ function populate() {
         studentForm.appendChild(form);
       }
     }
+
+function checkPassword() {
+  if (document.getElementById('password').value != '') {
+    if (document.getElementById('password').value ==
+    document.getElementById('confirm_password').value) {
+      document.getElementById('submit').disabled = false;
+  } else {
+    document.getElementById('submit').disabled = true;
+    alert("Password doesn't match")
+    }
+  } else {
+    document.getElementById('submit').disabled = true;
+  }
+}
+
+function validatePassword(password)
+{
+    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    if(!re.test(password)) {
+      alert('Password must contain at least 8 characters\nOne symbol\nAn uppercase letter\nA lowercase letter\nAnd a number')
+    }
+}
