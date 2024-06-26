9bebe74aaee62bc6bf967851248f0622dd8a0bc9
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-06-26 15:09:54 +0800

    Create registration.php

---
+++ php/registration.php
@@ -0,0 +1,14 @@
+<?php
+
+echo 'Hello from registration.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $first_name = $_POST["first_name"];
+  $username = $_POST["username"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+
+  echo $first_name;
+  // Use the values as needed
+  // ...
+}
