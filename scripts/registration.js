d50dd6009dd9f0ec1b28ee219732f561e43dbbeb
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-06-30 18:47:58 +0800

    Update registration.js

--- scripts/registration.js
+++ scripts/registration.js
@@ -10,7 +10,27 @@ function showForm() {
   footer.style= "margin-top: 10px";
 }
 
+var pwInput = document.getElementById("password");
+var lower = document.getElementById("lower");
+var upper = document.getElementById("upper");
+var number = document.getElementById("number");
+var symbol = document.getElementById("symbol");
+var minlength = document.getElementById("minlength");
+
+// When the user clicks on the password field, show the message box
+pwInput.cha = function() {
+  document.getElementById("passwordMessage").style.display = "block";
+}
+
+pwInput.addEventListener("keyup", () => {});
+
+// When the user clicks outside of the password field, hide the message box
+pwInput.onblur = function() {
+  document.getElementById("passwordMessage").style.display = "none";
+}
+
 function checkPassword() {
+
   if (document.getElementById('password').value != '') {
     if (document.getElementById('password').value ==
     document.getElementById('confirm_password').value) {
@@ -26,8 +46,34 @@ function checkPassword() {
 
 function validatePassword(password)
 {
-    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    if(!re.test(password)) {
-      alert('Password must contain at least 8 characters\nOne symbol\nAn uppercase letter\nA lowercase letter\nAnd a number')
+    var noUpper = /^(?=.*[A-Z])/;
+    var noLower = /^(?=.*[a-z])/;
+    var noNum = /^(?=.*\d)/;
+    var noSymbol = /^(?=.*[!@#$%^&*])/;
+    var minLength = /^.{8,}$/;
+
+    if(noUpper.test(password)) {
+      upper.classList.remove("invalid");
+      upper.classList.add("valid");
+    }
+
+    if(noLower.test(password)) {
+      lower.classList.remove("invalid");
+      lower.classList.add("valid");
+    }
+
+    if(noNum.test(password)) {
+      number.classList.remove("invalid");
+      number.classList.add("valid");
+    }
+
+    if(noSymbol.test(password)) {
+      symbol.classList.remove("invalid");
+      symbol.classList.add("valid");
+    }
+
+    if(minLength.test(password)) {
+      minlength.classList.remove("invalid");
+      minlength.classList.add("valid");
     }
 }
