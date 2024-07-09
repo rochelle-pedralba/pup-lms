4d47b93259bc4f7013a1bc2a8df53dcb236de482
Nicole Jewel <156534679+mieffee@users.noreply.github.com>
2024-07-09 23:53:59 +0800

    Create admin_overview.js

---
+++ scripts/admin_overview.js
@@ -0,0 +1,18 @@
+function createSubject(){
+    window.location.href = '../../../pages/create_subject.html' //change to the real path
+}
+function updateSubject(){
+    window.location.href = '../../../pages/update_subject.html' //change to the real path
+}
+function updateCourse(){
+    window.location.href = '../../../pages/update_course.html' //change to the real path
+}
+function updateCohort(){
+    window.location.href = '../../../pages/update_cohort.html' //change to the real path
+}
+function updateDepartment(){
+    window.location.href = '../../../pages/update_department.html' //change to the real path
+}
+function enrollStudent(){
+    window.location.href = '../../../pages/enroll_student.html' //change to the real path
+}
