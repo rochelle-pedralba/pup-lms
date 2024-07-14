8c2faca484cfd634739540fd5ecc7c769770e394
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-07-14 21:13:07 +0800

    Update update_student_course.php

--- pages/admin/course/update_student_course.php
+++ pages/admin/course/update_student_course.php
@@ -78,5 +78,8 @@
     </div>
     </div>
 
+    <script src="../../../scripts/unenroll_course.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
   </body>
 </html>
