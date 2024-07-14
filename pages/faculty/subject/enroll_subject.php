38b0e57aaa288a7a9c7e118d56481e5551354798
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-07-14 19:06:55 +0800

    Create enroll_subject.php

---
+++ pages/faculty/subject/enroll_subject.php
@@ -0,0 +1,62 @@
+<?php 
+  session_start();
+
+  $_SESSION["course_ID"] = "BSCS";
+  $_SESSION["subject_ID"] = "COMP10173";
+  $_SESSION["cohort_ID"] = "PUPSJ";
+  $_SESSION["ay"] = "2324";
+  $_SESSION["semester"] = "2";
+  $_SESSION["year"] = "3";
+  $_SESSION["section"] = "5";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="../../../styles/update_class.css">
+  </head>
+
+  <body>
+    <div>
+      <div>
+        <h4>Professor Name</h4>
+      </div>
+      <div>
+        <h3 class="year-section-container">Year and Section</h3>
+        <h1>Subject ID: Subject Name</h1>
+        <h3>Semester  A.Y.</h3>
+      </div>
+    </div>
+
+    <div id="enroll_students">
+      <div class="first-row">
+        <!-- Display all BSCS students -->
+        <div id="search_student">
+        <form method="POST">
+          <input type="text" id="studentSearch" name="studentID" placeholder="Enter Student ID">
+          <button type="submit" id="searchBtn">Search</button>
+        </form>
+        <div id="searchedStudentInfo"></div>
+
+          <div class="display-student">
+            <div id="student_list">
+              <?php
+                require_once '../../../php/enrolled_student_list.php'; 
+              ?>
+            </div>
+          </div>
+        </div>
+
+        <div class="selected-student-container">
+          <div class="title">
+            <h3>Selected Student</h3>
+            <button id="enroll_student">Enroll</button>
+          </div>
+          <div id="selected_student"></div>
+        </div>
+    </div>
+
+    <div id="unenroll_students">
+    </div>
+  </body>
+</html>
