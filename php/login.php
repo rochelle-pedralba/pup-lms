2f97791349fb50b427dbd3af870662c80af58da1
WenzelBolo <wenzelandrei.bolo@gmail.com>
2024-07-14 23:47:15 +0800

    added: password maintenance feature that will lock account for 1 hour after 3 attempst

--- php/login.php
+++ php/login.php
@@ -10,10 +10,11 @@ require_once 'includes/error_model_inc.php';
 
 function getUser(object $mysqli, string $user_ID): ?array
 {
-    $query = "SELECT ua.*, ui.account_Status, ur.user_Role, ua.first_Access
+    $query = "SELECT ua.*, ui.account_Status, ur.user_Role, ua.first_Access, pm.login_Attempt
               FROM USER_ACCESS ua
               JOIN USER_INFORMATION ui ON ua.user_ID = ui.user_ID
               LEFT JOIN USER_ROLE ur ON ua.user_ID = ur.user_ID
+              LEFT JOIN PASSWORD_MAINTENANCE pm ON ua.user_ID = pm.user_ID
               WHERE ua.user_ID = ? 
               AND (ui.account_Status = ? OR ui.account_Status = ?)";
 
@@ -65,19 +66,64 @@ function setFirstAccess(object $mysqli, string $user_ID)
     }
 }
 
+function updateLoginAttempt(object $mysqli, string $user_ID, int $attempts)
+{
+    $query = "UPDATE PASSWORD_MAINTENANCE SET login_Attempt = ? WHERE user_ID = ?";
+    $params_update_attempt = [$attempts, $user_ID];
+    $update_result = executeQuery($mysqli, $query, "is", $params_update_attempt);
+
+    if (!$update_result['success']) {
+        $error_message = "An internal error has occured. Please try again later or contact the administrator.";
+        redirectWithError($error_message);
+        exit;
+    }
+}
+
+function isAccountLocked(object $mysqli, string $user_ID): bool
+{
+    $query = "SELECT login_Attempt, last_Access, time_Access FROM PASSWORD_MAINTENANCE WHERE user_ID = ?";
+    $queryResult = executeQuery($mysqli, $query, "s", [$user_ID]);
+
+    if (!$queryResult['success']) {
+        $error_message = "An internal error has occured. Please try again later or contact the administrator.";
+        redirectWithError($error_message);
+        exit;
+    }
+
+    $result = $queryResult['result']->fetch_assoc();
+
+    if ($result['login_Attempt'] >= 3) {
+        $lastAccessTime = strtotime($result['last_Access'] . ' ' . $result['time_Access']);
+        if ((time() - $lastAccessTime) < 3600) {
+            return true;
+        } else {
+            updateLoginAttempt($mysqli, $user_ID, 0);
+        }
+    }
+    return false;
+}
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $user_ID = $_POST['user_ID'];
     $password = $_POST["password"];
 
-    $user = getUser($mysqli, $user_ID);
-
-    if ($user === null || !verifyPassword($password, $user)) {
-        echo "<script>alert('Password incorrect. Please try again.');
+    if (isAccountLocked($mysqli, $user_ID)) {
+        echo "<script>alert('Account locked due to too many failed login attempts. Please try again after 1 hour.');
               window.location.href = '../pages/login.html'; 
               </script>";
         exit;
     }
 
+    $user = getUser($mysqli, $user_ID);
+
+    if ($user === null || !verifyPassword($password, $user)) {
+        $loginAttempts = $user['login_Attempt'] ?? 0;
+        updateLoginAttempt($mysqli, $user_ID, $loginAttempts + 1);
+        echo "<script>alert('Password incorrect. Please try again.');
+              window.location.href = '../pages/login.html'; 
+              </script>";
+        exit;
+    }
 
     if ($user['account_Status'] != '1') {
         $error_message = "Your account must be activated. Please contact the administrator.";
