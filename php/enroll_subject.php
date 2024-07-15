e0a5260dc4dfb20d90558e78044157ab86ea1bde
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-07-15 21:48:15 +0800

    Update enroll_subject.php

--- php/enroll_subject.php
+++ php/enroll_subject.php
@@ -4,24 +4,15 @@ require_once 'includes/dbh_inc.php';
 require_once 'includes/execute_query_inc.php';
 require_once 'includes/error_model_inc.php';
 
-session_start();
+$course_ID = $_SESSION["course_ID"];
+$subjectID = $_SESSION["subject_ID"];
+$cohortID = $_SESSION["cohort_ID"];
+$ay = $_SESSION["ay"];
+$semester = $_SESSION["semester"];
+$year = $_SESSION["year"];
+$section = $_SESSION["section"];
 
-$courseID = isset($_SESSION["course_ID"]) ? $_SESSION["course_ID"] : null;
-$subjectID = isset($_SESSION["subject_ID"]) ? $_SESSION["subject_ID"] : null;
-$cohortID = isset($_SESSION["cohort_ID"]) ? $_SESSION["cohort_ID"] : null;
-$ay = isset($_SESSION["ay"]) ? $_SESSION["ay"] : null;
-$semester = isset($_SESSION["semester"]) ? $_SESSION["semester"] : null;
-$year = isset($_SESSION["year"]) ? $_SESSION["year"] : null;
-$section = isset($_SESSION["section"]) ? $_SESSION["section"] : null;
-
-if (!$courseID || !$subjectID || !$cohortID || !$ay || !$semester || !$year || !$section) {
-  // Redirect to a previous step or show an error message
-  die('Error: Required session variables are not set.');
-}
-
-var_dump($_SESSION);
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['studentID'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SESSION['course_ID'])) {
   $data = json_decode(file_get_contents('php://input'), true);
 
   // Assuming the action is to enroll students
