1807f057c409907406d24dfb26dcc7f57c9220d6
jobenriquez <jobiskits@gmail.com>
2024-07-16 21:11:19 +0800

    ..

--- php/includes/dbh_inc.php
+++ php/includes/dbh_inc.php
@@ -1,12 +1,12 @@
 <?php
-    $dbhost = "localhost:3307";
-    $dbuser = "root";
-    $dbpass = "";
-    $dbname = "pup_lms";
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "pup_lms";
 
-    $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-    if (!is_null($mysqli->connect_error)) {
-        throw new Exception('Connection failed: ' . $mysqli->connect_error);
-    }
+if (!is_null($mysqli->connect_error)) {
+    throw new Exception('Connection failed: ' . $mysqli->connect_error);
+}
 ?>
