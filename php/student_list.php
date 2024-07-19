9173f2537c250bbf10836f96942500404198441e
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-07-20 00:03:38 +0800

    Update student_list.php

--- php/student_list.php
+++ php/student_list.php
@@ -3,12 +3,12 @@
   require_once 'includes/dbh_inc.php';
   require_once 'includes/execute_query_inc.php';
   require_once 'includes/error_model_inc.php';
-  
-  $courseID = isset($_SESSION["course_ID"]) ? $_SESSION["course_ID"] : null;
-  $cohortID = isset($_SESSION["cohort_ID"]) ? $_SESSION["cohort_ID"] : null;
 
   if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['studentID'])) {
     
+    $courseID = isset($_GET['course_ID']) ? $_GET['course_ID'] : null;
+    $cohortID = isset($_GET['cohort_ID']) ? $_GET['cohort_ID'] : null;
+
     $studentID = $_POST['studentID'];
     $query = "SELECT ui.user_ID, ui.email_Address, ui.first_Name, ui.last_Name
           FROM user_information ui
