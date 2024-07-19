e9c9394410e5a533bcba19a139800d058344f7ad
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-07-20 00:28:32 +0800

    Update student_list.php

--- php/student_list.php
+++ php/student_list.php
@@ -4,6 +4,9 @@
   require_once 'includes/execute_query_inc.php';
   require_once 'includes/error_model_inc.php';
 
+  $courseID = isset($_SESSION['course_ID']) ? $_SESSION['course_ID'] : null;
+  $cohortID = isset($_SESSION['cohort_ID']) ? $_SESSION['cohort_ID'] : null;
+
   if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['studentID'])) {
     
     $courseID = isset($_GET['course_ID']) ? $_GET['course_ID'] : null;
