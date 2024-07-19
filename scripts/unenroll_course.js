dcb3ecb7afaff8b5622f999967100e75c0bf9094
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-07-20 00:28:34 +0800

    Update unenroll_course.js

--- scripts/unenroll_course.js
+++ scripts/unenroll_course.js
@@ -1,7 +1,7 @@
 function unenrollFunc(studentName) {
     var unenroll = confirm("Are you sure you want to unenroll " + studentName + "?");
     if (unenroll) {
-        var encodedStudentName = encodeURIComponent(studentName);  
+        var encodedStudentName = encodeURIComponent(studentName);
         // Redirect to PHP script with encoded studentName as a query parameter
         window.location.href = '../../../php/archive_enrolled_course.php?studentName=' + encodedStudentName;
     }
