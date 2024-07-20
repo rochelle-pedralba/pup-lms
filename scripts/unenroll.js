0590e13bf8f0c1e81d5d8162c6280f34437f49f1
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-07-20 09:57:43 +0800

    Update unenroll.js

--- scripts/unenroll.js
+++ scripts/unenroll.js
@@ -1,8 +1,28 @@
-function unenrollFunc(studentName) {
+function unenrollFunc(studentName, studentID) {
     var unenroll = confirm("Are you sure you want to unenroll " + studentName + "?");
     if (unenroll) {
-        var encodedStudentName = encodeURIComponent(studentName);  
-        // Redirect to PHP script with encoded studentName as a query parameter
-        window.location.href = '../../../php/archive_enrolled.php?studentName=' + encodedStudentName;
+
+        if (studentID) {
+            try {
+
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', '../../../php/archive_enrolled.php?studentName=', true);
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+                xhr.onreadystatechange = function() {
+                    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                        //window.location.reload();
+                    }
+                }
+                xhr.send('studentID=' + encodeURIComponent(studentID));
+
+            } catch (e) {
+                console.error("Error posting data: ", e);
+
+                return;
+            }
+            } else {
+                alert("No selected students.");
+            }
     }
 }
